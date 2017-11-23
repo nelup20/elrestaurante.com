9afb6a36c1a148f56add20d5a5ca7ccb9fcb00b2
Nelu Platonov <platon.nelu@yahoo.com>
2017-11-23 19:58:33 +0100

    Basically finished Homepage for mobile, I know the intendation in the html file is horrible at the menuList part. Ill fix it later :^)

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,10 +1,14 @@
 var hamburgerIcon = document.querySelector(".fa.fa-bars");
+var hamburgerIconBackground = document.querySelector(".hamburgerBackground");
 var navLinks = document.querySelector(".navigationLinks");
-var navOpacity = 0
+var navOpacity = 0;
+var iconBackgroundColor = "rgba(255,255,255,0.2)";
 
 function showMenu(){
     navOpacity === 0 ? navOpacity = 1 : navOpacity = 0;
+    navOpacity === 1 ? iconBackgroundColor = "rgba(255,255,255,0.2)" : iconBackgroundColor = "none";
+    navOpacity === 1 ? hamburgerIcon.style.transform = "rotate(90deg)" : hamburgerIcon.style.transform = "rotate(0deg)";
+    hamburgerIconBackground.style.background = iconBackgroundColor;
     navLinks.style.opacity = navOpacity;
-
 }
 hamburgerIcon.addEventListener("click", showMenu);
