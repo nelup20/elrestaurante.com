4e7646ac2b0c6ea168973899123ae49d4a830f52
Nelu Platonov <platon.nelu@yahoo.com>
2017-11-26 20:26:05 +0100

    Added scroll event for fade-in type effect of some elements. Next i need to do is take care of the layout/design of the menu, after that test and fix on 1920x1080, after that cross-browser compatibility, then time to work on the other pages

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,6 +3,10 @@ var hamburgerIconBackground = document.querySelector(".hamburgerBackground");
 var navLinks = document.querySelector(".navigationLinks");
 var navOpacity = 0;
 var iconBackgroundColor = "rgba(255,255,255,0.2)";
+var reservePart = document.querySelector(".reservationPart");
+var aboutPart = document.querySelector(".aboutPart");
+var firstTwoSpecialities = document.querySelector("#firstTwoSpecialities");
+var lastTwoSpecialities = document.querySelector("#lastTwoSpecialities");
 
 function showMenu(){
     navOpacity === 0 ? navOpacity = 1 : navOpacity = 0;
@@ -10,5 +14,20 @@ function showMenu(){
     navOpacity === 1 ? hamburgerIcon.style.transform = "rotate(90deg)" : hamburgerIcon.style.transform = "rotate(0deg)";
     hamburgerIconBackground.style.background = iconBackgroundColor;
     navLinks.style.opacity = navOpacity;
-}
+};
+
+function revealReservation(e){
+    setTimeout(function(){
+        if(window.pageYOffset >= 860){
+            reservePart.style.opacity = 1;
+            aboutPart.style.opacity = 1;
+        };
+        if(window.pageYOffset >= 2800){
+            firstTwoSpecialities.style.opacity = 1;
+            lastTwoSpecialities.style.opacity = 1;
+        }
+    }, 150);
+};
+
 hamburgerIcon.addEventListener("click", showMenu);
+window.addEventListener("scroll", revealReservation)
