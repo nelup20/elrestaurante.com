540d0a9515b558014f63d781e354f60b1c9f8ea6
Nelu Platonov <platon.nelu@yahoo.com>
2018-01-15 21:46:31 +0100

    Set the 1st set of divs/cards that fade in to just fade in when the window is loaded. Once im done with all the regular css for all viewports need to make good looking animations for a lot of stuff

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,19 +4,18 @@ var aboutPart = document.querySelector(".aboutPart");
 var firstTwoSpecialities = document.querySelector("#firstTwoSpecialities");
 var lastTwoSpecialities = document.querySelector("#lastTwoSpecialities");
 
+window.onload = function(){
+    reservePart.style.opacity = 1;
+    setTimeout(function(){
+        aboutPart.style.opacity = 1;
+    }, 500);
+}
+
 function revealReservation(e){
     setTimeout(function(){
-        if(window.innerHeight < 900 && window.pageYOffset >= 860){
-            reservePart.style.opacity = 1;
-            aboutPart.style.opacity = 1;
-        };
-        if(window.innerHeight < 900 && window.pageYOffset >= 2800){
+        if(window.innerHeight < 900 && window.pageYOffset >= 1400){
             firstTwoSpecialities.style.opacity = 1;
             lastTwoSpecialities.style.opacity = 1;
-        };
-        if(window.innerHeight > 900 && window.pageYOffset >= 900){
-            reservePart.style.opacity = 1;
-            aboutPart.style.opacity = 1;
         };
         if(window.innerHeight > 900 && window.pageYOffset >= 3500){
             firstTwoSpecialities.style.opacity = 1;
