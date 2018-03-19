aecdb3b39a70d785ca16fa1387710b464019ee3d
Nelu Platonov <platon.nelu@yahoo.com>
2018-03-19 21:29:57 +0100

    Added sweetalert + animate.css, id say homepage is basically done. Need to just add media queries & take care of all screen sizes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,27 +1,36 @@
-
 var reservePart = document.querySelector(".reservationPart");
 var aboutPart = document.querySelector(".aboutPart");
-var firstTwoSpecialities = document.querySelector("#firstTwoSpecialities");
-var lastTwoSpecialities = document.querySelector("#lastTwoSpecialities");
-
-window.onload = function(){
-    reservePart.style.opacity = 1;
-    setTimeout(function(){
-        aboutPart.style.opacity = 1;
-    }, 700);
-}
-
-function revealReservation(e){
-    setTimeout(function(){
-        if(window.innerHeight < 900 && window.pageYOffset >= 1400){
-            firstTwoSpecialities.style.opacity = 1;
-            lastTwoSpecialities.style.opacity = 1;
-        };
-        if(window.innerHeight > 900 && window.pageYOffset >= 3500){
-            firstTwoSpecialities.style.opacity = 1;
-            lastTwoSpecialities.style.opacity = 1;
-        }
-    }, 150);
+var firstTwoSpecialities = document.getElementById("firstTwoSpecialities");
+var lastTwoSpecialities = document.getElementById("lastTwoSpecialities");
+var bookBtn = document.getElementById("book-table-button");
+var menu = document.getElementsByClassName("menu")[0];
+window.onload = function() {
+  reservePart.style.opacity = 1;
+  setTimeout(function() {
+    aboutPart.style.opacity = 1;
+  }, 700);
 };
 
-window.addEventListener("scroll", revealReservation)
+function reveal(e) {
+  var timeout = setTimeout(function() {
+    if (window.innerHeight < 900 && window.pageYOffset >= 1400 || window.innerHeight > 900 && window.pageYOffset >= 3500) {
+      firstTwoSpecialities.style.opacity = 1;
+      lastTwoSpecialities.style.opacity = 1;
+    }
+    if (window.innerHeight < 900 && window.pageYOffset >= 2300) {
+       menu.style.opacity = "1"
+       menu.classList = "menu animated fadeInUp";
+      }
+  }, 150);
+}
+
+bookBtn.addEventListener("click", function(){
+    swal({
+        title: 'Sorry!',
+        text: 'All tables are full, please try another time',
+        type: 'error',
+        confirmButtonText: 'Ok'
+      })
+})
+
+window.addEventListener("scroll", reveal);
